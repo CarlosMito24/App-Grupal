a52bb7de7daf5727544010f4ffba8ab96d66862c
XxFJxX <ferinocente357@gmail.com>
2023-07-10 09:47:13 -0600

    Hola

--- App Grupal/SueldoSemanal.xaml.cs
+++ App Grupal/SueldoSemanal.xaml.cs
@@ -10,28 +10,20 @@ public partial class SueldoSemanal : ContentPage
     private void Button_Clicked(object sender, EventArgs e)
     {
 
-        if (!string.IsNullOrEmpty(ht.Text))
+        if (!string.IsNullOrEmpty(ht.Text) && !string.IsNullOrEmpty(ph.Text))
         {
-            if (!string.IsNullOrEmpty(ph.Text))
-            {
-                string total;
-                double ht1 = Convert.ToDouble(ht.Text);
-                double ph1 = Convert.ToDouble(ph.Text);
-                double p = (ht1 * ph1);
-                total = p.ToString("0.00");
+            string total;
+            double ht1 = Convert.ToDouble(ht.Text);
+            double ph1 = Convert.ToDouble(ph.Text);
+            double p = (ht1 * ph1);
+            total = p.ToString("0.00");
 
-                la.Text = Convert.ToString("$ " + total);
-            }
-            else
-            {
-                DisplayAlert("Error", "Introduce los datos solicitados", "Ok");
-
-            }
+            la.Text = Convert.ToString("$ " + total);
         }
         else
         {
             DisplayAlert("Error", "Introduce los datos solicitados", "Ok");
-            
+            la.Text = "";
         }
        
     }
