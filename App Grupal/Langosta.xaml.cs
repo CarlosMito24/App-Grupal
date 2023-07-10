3a0c01cefb3370cfa8bca4cfea32d53d53c3d325
XxFJxX <ferinocente357@gmail.com>
2023-07-10 09:36:49 -0600

    Fix

--- App Grupal/Langosta.xaml.cs
+++ App Grupal/Langosta.xaml.cs
@@ -6,4 +6,34 @@ public partial class Langosta : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+        if (!string.IsNullOrEmpty(np.Text))
+        {
+            int tt, p = Convert.ToInt32(np.Text);
+
+            if (p >= 300)
+            {
+                tt = (p * 75);
+            }
+            else if (p > 200)
+            {
+                tt = (p * 85);
+            }
+            else
+            {
+                tt = (p * 95);
+            }
+
+            la.Text = Convert.ToString("$ " + tt);
+        }
+        else
+        {
+            DisplayAlert("Error", "Introduce los datos solicitados", "Ok");
+
+        }
+        
+    }
 }
--- App Grupal/SueldoSemanal.xaml.cs
+++ App Grupal/SueldoSemanal.xaml.cs
@@ -9,12 +9,30 @@ public partial class SueldoSemanal : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        string total;
-		double ht1 = Convert.ToDouble(ht.Text);
-        double ph1 = Convert.ToDouble(ph.Text);
-        double p = (ht1 * ph1);
-        total = p.ToString("0.00");
 
-        la.Text = Convert.ToString("$ " + total);
+        if (!string.IsNullOrEmpty(ht.Text))
+        {
+            if (!string.IsNullOrEmpty(ph.Text))
+            {
+                string total;
+                double ht1 = Convert.ToDouble(ht.Text);
+                double ph1 = Convert.ToDouble(ph.Text);
+                double p = (ht1 * ph1);
+                total = p.ToString("0.00");
+
+                la.Text = Convert.ToString("$ " + total);
+            }
+            else
+            {
+                DisplayAlert("Error", "Introduce los datos solicitados", "Ok");
+
+            }
+        }
+        else
+        {
+            DisplayAlert("Error", "Introduce los datos solicitados", "Ok");
+            
+        }
+       
     }
 }
